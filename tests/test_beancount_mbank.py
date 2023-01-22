60c5e1e03b32784991d687faab422bb89132142a
Łukasz Strzępek <lstrzepek@users.noreply.github.com>
2023-01-22 20:38:16 +0100

    Fixed initial test

--- tests/test_beancount_mbank.py
+++ tests/test_beancount_mbank.py
@@ -4,4 +4,4 @@ from beancount_mbank.mbank import MBankImporter
 
 def test_main():
     importer = MBankImporter(account="Assets:MBank:Debit")
-    assert importer.name() == "MBankImporter: Assets:MBank:Debit"
+    assert importer.name() == 'beancount_mbank.mbank.MBankImporter: "Assets:MBank:Debit"'
